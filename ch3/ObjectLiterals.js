c9405f9160bd6fe9b35ff58a9112493fc422ad15
Rainier F. Go <rainier.f.go@gmail.com>
2017-01-24 23:55:48 -0500

    Finished ch3

--- ch3/ObjectLiterals.js
+++ ch3/ObjectLiterals.js
@@ -26,3 +26,59 @@ console.log(flight.departure.IATA);
 console.log(stooge["middle-name"]); 
 console.log(flight.status); 
 console.log(stooge["FIRST-NAME"]);  
+
+stooge['first-name'] = 'Jerome'; 
+
+stooge['middle-name'] = 'Curly'; 
+stooge.nickname = 'Curly'; 
+flight.equipment = {
+	model: 'Boeing 777'
+}; 
+
+flight.status = 'overdue'; 
+
+//Objects are passed around by reference. They are never copied. 
+
+if (typeof Object.create !== 'function') {
+		Object.create = function (o) {
+			var F = function () {}; 
+			F.prototype = o; 
+			return new F(); 
+		}; 
+}
+
+var another_stooge = Object.create(stooge); 
+
+console.log(another_stooge); 
+console.log(stooge); 
+
+console.log(typeof flight.number); 
+console.log(typeof flight.status); 
+
+console.log(typeof flight.constructor); 
+console.log(flight.hasOwnProperty('constructor')); 
+
+var name; 
+for (name in another_stooge) {
+	console.log(name + ':' + another_stooge[name]); 
+}
+
+var i; 
+var properties = [
+	'first-name', 
+	'middle-name', 
+	'last-name', 
+	'profession'
+]; 
+for (i = 0; i < properties.length; i += 1) {
+	console.log(properties[i] + ':' + another_stooge[properties[i]]); 
+}
+
+another_stooge.nickname = 'Moe'; 
+console.log(another_stooge.nickname);
+delete another_stooge.nickname; 
+console.log(another_stooge.nickname);
+
+//Javascript makes it easy to define global variables that can hold all of the assets of 
+//your application. Unfortunately, global variables weaken the resiliency of programs 
+//and should be avoided. 
